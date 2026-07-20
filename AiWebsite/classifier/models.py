48a653dbface7c8867e39a61a8f4be5a616480da
AnwarAldahan <noorrdhncoll@gmail.com>
2026-07-20 14:29:22 +0300

    Feat: Added a second model

--- AiWebsite/classifier/models.py
+++ AiWebsite/classifier/models.py
@@ -8,3 +8,19 @@ class PredictionResult(models.Model):
 
     def __str__(self):
         return f"Image {self.label} ({self.confidence:.2f})"
+    
+    class ResultStatus(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('finished_ai', 'Finished - AI Generated'),
+        ('finished_real', 'Finished - Real'),
+        ('failed', 'Failed'),
+    ]
+
+    result = models.ForeignKey(PredictionResult, on_delete=models.CASCADE, related_name='statuses')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    #caption = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.result_id} - {self.status}"
